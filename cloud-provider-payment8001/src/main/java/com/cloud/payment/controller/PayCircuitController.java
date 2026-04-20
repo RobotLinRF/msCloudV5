c99d4c27d4d80e4308f90820c6bb96d444a01fa9
LRF <2872547185@qq.com>
2026-04-20 11:21:12 +0800

    至circuitBreaker断路器

---
+++ cloud-provider-payment8001/src/main/java/com/cloud/payment/controller/PayCircuitController.java
@@ -0,0 +1,48 @@
+package com.cloud.payment.controller;
+
+import cn.hutool.core.util.IdUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+@RestController
+public class PayCircuitController {
+    //Resilience4J CircuitBreaker断路器
+
+    @GetMapping(value = "/pay/circuit/{id}")
+    public String payCircuit(@PathVariable("id") Integer id) {
+        if (id == -4) throw new RuntimeException("------id 不能为负数");
+        if (id == 999) {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return "circuit pay inputId: " + id + "\t" + IdUtil.simpleUUID();
+    }
+
+    @GetMapping(value = "/pay/bulkhead/{id}")
+    public String myBulkhead(@PathVariable("id") Integer id) {
+        if (id == -4) throw new RuntimeException("----bulkhead id 不能-4");
+
+        if (id == 9999) {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return "Hello, bulkhead! inputId:  " + id + " \t " + IdUtil.simpleUUID();
+    }
+
+    //=========Resilience4j ratelimit 的例子
+    @GetMapping(value = "/pay/ratelimit/{id}")
+    public String myRatelimit(@PathVariable("id") Integer id)
+    {
+        return "Hello, myRatelimit欢迎到来 inputId:  "+id+" \t " + IdUtil.simpleUUID();
+    }
+}
